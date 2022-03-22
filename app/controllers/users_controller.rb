86f40b7c6235439024274d31565a511c0b0f42fb
Karel van Oordt <vanoordtkarel@gmail.com>
2022-03-22 10:27:36 -0500

    Update routes and install linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,6 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!, except: :index
+
     def index
-    end
-
-    def show
-    end
-
-    def new
-    end
-
-    def create
     end
 end
